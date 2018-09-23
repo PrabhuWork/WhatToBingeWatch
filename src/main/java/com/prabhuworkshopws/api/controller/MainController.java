f5e7297f77d1588a86b3cd984527bbec5dde37ef
unknown <pratik.bhuite@gmail.com>
2018-09-23 20:43:53 +0530

    hello

--- src/main/java/com/prabhuworkshopws/api/controller/MainController.java
+++ src/main/java/com/prabhuworkshopws/api/controller/MainController.java
@@ -5,6 +5,7 @@ package com.prabhuworkshopws.api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author PraBhu
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class MainController {
-	
-	
-	@RequestMapping(value="/getMovieData")
+
+	@RequestMapping(value = "/getMovieData")
 	public String getMovieData() {
+
+		RestTemplate restTemplate = new RestTemplate();
+        String quote = restTemplate.getForObject("https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=366a57c52697a800b7778f5aa0bb3841", String.class);
 		
-		return "hello dude";
+        return quote;
 	}
 
 }

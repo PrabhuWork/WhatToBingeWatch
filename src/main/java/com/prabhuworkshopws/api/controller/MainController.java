cf0aac51bffddea1ec23563deac60abfa6329109
unknown <pratik.bhuite@gmail.com>
2018-09-23 20:48:03 +0530

    hello

--- src/main/java/com/prabhuworkshopws/api/controller/MainController.java
+++ src/main/java/com/prabhuworkshopws/api/controller/MainController.java
@@ -3,6 +3,7 @@
  */
 package com.prabhuworkshopws.api.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -14,11 +15,11 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MainController {
 
-	@RequestMapping(value = "/getMovieData")
-	public String getMovieData() {
+	@RequestMapping(value = "/getMovieData/{pageId}")
+	public String getMovieData(@PathVariable(value="pageId") Integer pageId) {
 
 		RestTemplate restTemplate = new RestTemplate();
-        String quote = restTemplate.getForObject("https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=366a57c52697a800b7778f5aa0bb3841", String.class);
+        String quote = restTemplate.getForObject("https://api.themoviedb.org/3/discover/movie?page="+pageId+"&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=366a57c52697a800b7778f5aa0bb3841", String.class);
 		
         return quote;
 	}
